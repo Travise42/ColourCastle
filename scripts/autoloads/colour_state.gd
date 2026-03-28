1afeedb732ef7e8aa9f7c8cdb9b80e89d3572bf1
2192375d <173829746yzpq@gmail.com>
2026-03-27 21:54:36 -0400

    made level exit + entrance

--- scripts/autoloads/colour_state.gd
+++ scripts/autoloads/colour_state.gd
@@ -2,13 +2,14 @@ extends Node
 
 var color_dictionary: Dictionary[Enums.ColorEnum, Color] = {
 	Enums.ColorEnum.WHITE: Color(1, 1, 1),
-	Enums.ColorEnum.RED: Color(1, 0, 0.3),
-	Enums.ColorEnum.BLUE: Color.SKY_BLUE,
-	Enums.ColorEnum.GREEN: Color.PALE_GREEN,
-	Enums.ColorEnum.PINK: Color.PINK,
+	Enums.ColorEnum.RED: Color(1, 0.4, 0.4),
+	Enums.ColorEnum.YELLOW: Color(1, 1, 0.3),
+	Enums.ColorEnum.GREEN: Color(0, 1, 0.3),
+	Enums.ColorEnum.PINK: Color(1, 0.2, 0.6),
 }
 
 func set_color(CSG_shape: CSGShape3D, color_enum: Enums.ColorEnum) -> void:
 	var material: Material = StandardMaterial3D.new()
 	material.albedo_color = color_dictionary[color_enum]
 	CSG_shape.material_override = material
+	
---
+++ scripts/level_connector.gd
@@ -0,0 +1,17 @@
+extends CSGCombiner3D
+
+class_name LevelConnector
+
+@export var csg_torus_3d: CSGTorus3D
+
+func _process(delta: float) -> void:
+	csg_torus_3d.rotate_y(delta)
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player:
+		print("player entered: ", self)
+		# move on to the previous/next level
+		pass
+
+func _to_string() -> String:
+	return name
